866c8fe91dcdb5a638f67c0b8bb4b49f0c847de3
Daniel Quelali <daniel.quelali@etermax.com>
2019-04-27 04:38:49 +0000

    Removed df_match_stats direct dependency on df_match_events

--- R/make_input_match_stats.R
+++ R/make_input_match_stats.R
@@ -123,34 +123,22 @@ get_df_time_played <- function(df_raw_data, df_match_events) {
 
 # Stuff -------------------------------------------------------------------
 
-get_df_kills <- function(df_match_events) {
-  df_match_events %>%
-    filter(event_type == 'kill') %>%
-    filter(event_properties.killer != 'NONE') %>%
+get_df_kills <- function(df_kill_events) {
+  df_kill_events %>%
+    filter(killer_guid != 'NONE') %>%
     
     # No contar team kills
-    filter(!event_properties.team_kill) %>%
+    filter(!is_team_kill) %>%
     
-    count(event_properties.killer, event_properties.victim) %>%
-    
-    group_by(event_properties.killer) %>%
-    summarise(
-      kills = sum(n)
-    ) %>%
-    ungroup() %>%
-    select(player_guid = event_properties.killer, kills) %>%
+    count(killer_guid) %>%
+    select(player_guid = killer_guid, kills = n) %>%
     arrange(-kills)
 }
 
-get_df_deaths <- function(df_match_events) {
-  df_match_events %>%
-    filter(event_type == 'kill') %>%
-    group_by(event_properties.victim) %>%
-    summarise(
-      deaths = n()
-    ) %>%
-    ungroup() %>%
-    select(player_guid = event_properties.victim, deaths) %>%
+get_df_deaths <- function(df_kill_events) {
+  df_kill_events %>%
+    count(victim_guid) %>%
+    select(player_guid = victim_guid, deaths = n) %>%
     arrange(-deaths)
 }
 
@@ -165,12 +153,15 @@ get_df_player_team_flags <- function(df_time_played) {
 
 # Intermediate results ----------------------------------------------------
 
-get_df_flag_stats <- function(df_match_events, df_time_played, df_match_players) {
+get_df_flag_stats <- function(
+  df_flag_events,
+  df_time_played,
+  df_match_players
+) {
   df_match_players %>%
     select(player_guid) %>%
     left_join(
-      df_match_events %>%
-        filter(grepl('flag', event_type), event_type != 'flag_capture_time') %>%
+      df_flag_events %>%
         count(event_type, player_guid) %>%
         ungroup(),
       by = 'player_guid'
@@ -183,10 +174,10 @@ get_df_flag_stats <- function(df_match_events, df_time_played, df_match_players)
       value = n
     ) %>%
     mutate(
-      banderas_enemigas_tocadas = flag_dropped + flag_captured
+      banderas_enemigas_tocadas = flag_dropped + flag_capture_time
     ) %>%
     rename(
-      banderas_capturadas = flag_captured,
+      banderas_capturadas = flag_capture_time,
       banderas_droppeadas = flag_dropped,
       banderas_recuperadas = flag_returned
     ) %>%
@@ -205,13 +196,22 @@ get_df_flag_stats <- function(df_match_events, df_time_played, df_match_players)
     select(-banderas_droppeadas)
 }
 
-get_df_kills_stats <- function(df_match_events, df_time_played) {
-  df_raw_kill_stats <- full_join(
-    get_df_kills(df_match_events),
-    get_df_deaths(df_match_events),
-    by = 'player_guid'
-  ) %>%
-    full_join(
+get_df_kills_stats <- function(
+  df_kill_events,
+  df_time_played,
+  df_match_players
+) {
+  df_raw_kill_stats <- df_match_players %>%
+    select(player_guid) %>%
+    left_join(
+      get_df_kills(df_kill_events),
+      by = 'player_guid'
+    ) %>%
+    left_join(
+      get_df_deaths(df_kill_events),
+      by = 'player_guid'
+    ) %>%
+    left_join(
       df_time_played %>%
         select(player_guid, total_time_played),
       by = 'player_guid'
@@ -241,10 +241,15 @@ get_df_kills_stats <- function(df_match_events, df_time_played) {
 # Exported ----------------------------------------------------------------
 
 ## TODO: Get rid of df_match_events dependency 
-get_df_match_stats <- function(df_match_events, df_time_played, df_match_players) {
+get_df_match_stats <- function(
+  df_kill_events,
+  df_flag_events,
+  df_time_played,
+  df_match_players
+) {
   df_raw_match_stats <- inner_join(
-    get_df_kills_stats(df_match_events, df_time_played),
-    get_df_flag_stats(df_match_events, df_time_played, df_match_players),
+    get_df_kills_stats(df_kill_events, df_time_played, df_match_players),
+    get_df_flag_stats(df_flag_events, df_time_played, df_match_players),
     by = 'player_guid'
   )
   
